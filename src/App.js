1ab46a0ba0f3a4cacf6ab81c0340c3b03def4ade
Trecy-Malonza <trecymalonza@gmail.com>
2024-07-12 09:22:03 +0300

    created website

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-
-import Video from "./videos"; 
-
-  const App = () =>{
-
-
-import Portfolio from "./Portfolio";
-import "./index.css";
-function App() {
-  return (
-    <div className='App'>
-     <Portfolio/>
-
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
@@ -18,7 +5,11 @@ import LandingPage from './LandingPage';
 import HomePage from './HomePage';
 
 import Blog from './Blog/blog';
+import Portfolio from "./Portfolio";
 import Portfolio from './Portfolio';
+import Video from "./videos"; 
+
+
 function App() {
   return (
     <div>
@@ -27,6 +18,8 @@ function App() {
       <HomePage/>
       <Blog />
       <Portfolio/>
+      <Portfolio/>
+      <Video/>
 
 
     </div>
